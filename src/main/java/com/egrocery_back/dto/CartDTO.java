6bd93dd6a9be82aa7858aabcfa13ad653b772d00
João Batista Neto <netobatista090@gmail.com>
2025-04-13 21:03:30 -0300

    Criação do DTO do carrinho.

--- src/main/java/com/egrocery_back/dto/CartDTO.java
+++ src/main/java/com/egrocery_back/dto/CartDTO.java
@@ -1,4 +1,33 @@
 package com.egrocery_back.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CartDTO {
+
+    @JsonProperty("id")
+    private Integer id;
+
+    @NotNull(message = "Quantity is required.")
+    @Min(value = 1, message = "Quantity must be at least 1.")
+    @JsonProperty("quantity")
+    private Integer quantity;
+
+    @JsonProperty("created_at")
+    private Timestamp createdAt;
+
+    @NotNull(message = "User is required.")
+    @JsonProperty("user_id")
+    private UserDTO user;
+
+    @NotNull(message = "Product is required.")
+    @JsonProperty("product")
+    private ProductDTO product;
 }
