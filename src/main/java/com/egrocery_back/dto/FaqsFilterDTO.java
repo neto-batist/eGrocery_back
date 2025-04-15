f0fd3ec2c92905136d54aec3ff6641bf85da66c7
João Batista Neto <netobatista090@gmail.com>
2025-04-14 21:13:19 -0300

    Adição de Getters e Setters por o @Data do Lombook  não está funcionando corretamente.

--- src/main/java/com/egrocery_back/dto/FaqsFilterDTO.java
+++ src/main/java/com/egrocery_back/dto/FaqsFilterDTO.java
@@ -7,7 +7,23 @@ import lombok.Setter;
 @Setter
 public class FaqsFilterDTO {
 
+    @Getter
     private String question;
     private Boolean isFrequentlyAsked;  // Novo campo para filtrar por perguntas frequentes
 
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Boolean getIsFrequentlyAsked() {
+        return isFrequentlyAsked;
+    }
+
+    public void setFrequentlyAsked(Boolean frequentlyAsked) {
+        isFrequentlyAsked = frequentlyAsked;
+    }
 }
